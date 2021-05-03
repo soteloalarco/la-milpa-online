9539d16cdf95518a4c2131dd67e4da4f68232c5d
Rolando Sotelo <rolandosoteloalarcon@gmail.com>
2021-05-02 21:31:55 -0500

    initial commit, express app

---
+++ server/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './app.ts',
+  target : 'node',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new NodemonPlugin(),
+  ],
+};
